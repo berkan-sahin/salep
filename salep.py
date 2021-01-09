8e84151c9b2310c121adb863e017cce7903bf0bd
Berkan <tt.berkan092@gmail.com>
2021-01-09 17:28:59 +0300

    base bot

--- salep.py
+++ salep.py
@@ -2,7 +2,6 @@ import doviz_api
 import discord
 from discord.ext import commands
 import logging
-import re
 
 API_KEY = ""
 
@@ -26,14 +25,6 @@ async def döviz(ctx, currency: str):
         return
     
     await ctx.send(f"1 {currency_abbr} = {exchange_rate} {doviz_api.base_currency}")
-
-@salep.listen("on_message")
-async def schtupid(message):
-    if message.author.id == salep.user.id:
-        return
-
-    if re.search("(z|Z)oom", message.clean_content):
-        await message.channel.send("Zoom is schtupid!!!1")
 
 if __name__ == "__main__":
     token_file = open("TOKEN", "r")
