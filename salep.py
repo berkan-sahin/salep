0479b699515578ecd8369fd848e58c8b19f4f4c2
berkan <tt.berkan092@gmail.com>
2021-01-21 11:48:02 +0300

    More elaorate documentation

--- salep.py
+++ salep.py
@@ -1,15 +1,22 @@
+# Exchange rate functions
 import doviz_api
+# Discord API
 import discord
 from discord.ext import commands
-from pymongo import MongoClient  
+# MongoDB
+from pymongo import MongoClient
+# Misc. imports  
 from typing import Union
 import logging
 from random import choice
 
+# This is the API key for the exchange rate service
+# It will be read from the TOKEN file
 API_KEY = ""
 
 salep = commands.Bot(command_prefix="s!")
 
+# Initialize database
 client = MongoClient()
 db = client.salep
 
@@ -22,6 +29,12 @@ async def on_ready():
 
 @salep.command()
 async def döviz(ctx, currency: str):
+    """Reports the current exchange rate between a user-specified currency and the Turkish Lira
+    
+    Args:
+        ctx (discord.Context): Invocation context, provided by discord.py on function call
+        currency (str): The name or the three-letter code for the desired currency
+    """    
 
     try:
         exchange_rate, currency_abbr = doviz_api.get_exchange_rate(currency, API_KEY)
@@ -32,10 +45,22 @@ async def döviz(ctx, currency: str):
     await ctx.send(f"1 {currency_abbr} = {exchange_rate} {doviz_api.base_currency}")
 
 def extract_id(name: Union[discord.Member, str]):
+    """
+    A helper function for storing and querying Members in the database
+    """
     return name if type(name) == str else name.id
 
 @salep.command()
 async def add_quote(ctx, name: Union[discord.Member, str], quote: str):
+    """Adds a quote to the corresponding entry for the mentioned person and creates said entry
+    if the person doesn't exist
+
+    Args:
+        ctx (discord.Context): Invocation context, passed by discord.py automatically
+        name (Union[discord.Member, str]): The name or mention of the person the quote belongs to
+        quote (str): The quote to add
+    """
+
     if db.people.find_one({"name": extract_id(name), "guild": ctx.guild.id}) is None:
         person = {
             "name": extract_id(name),
@@ -52,6 +77,13 @@ async def add_quote(ctx, name: Union[discord.Member, str], quote: str):
 
 @salep.command()
 async def quote(ctx, name: Union[discord.Member, str]):
+    """Picks and posts a quote belonging to the specific person and guild at random from the database
+
+    Args:
+        ctx (discord.Context): Invocation context, passed by discord.py automatically
+        name (Union[discord.Member, str]): The name or mention of the person to pick a quote from
+    """
+
     person = db.people.find_one({"name": extract_id(name), "guild": ctx.guild.id})
     if person is None:
         await ctx.send("This person does not exist")
@@ -65,6 +97,7 @@ if __name__ == "__main__":
                         format="%(asctime)s %(levelname)-8s %(message)s",
                         datefmt="%Y-%m-%d %H:%M:%S")
 
+    # Read exchange API key and bot token from the TOKEN file.
     with open("TOKEN", "r") as f:
         token = f.readline().strip()
         API_KEY = f.readline().strip()
